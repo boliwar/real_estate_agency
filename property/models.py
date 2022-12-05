6cfd0ae53291ac686b85a4c6ef5317a6cd1fee15
boliwar <boliwar@gmail.com>
2022-12-05 18:01:34 +0200

    не останется закомментированого кода

--- property/models.py
+++ property/models.py
@@ -5,9 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    # owner = models.CharField('ФИО владельца', max_length=200)
-    # owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    # owner_pure_phone = PhoneNumberField(region="CA", verbose_name='Нормализованный номер телефона', blank=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
