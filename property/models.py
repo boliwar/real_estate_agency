127bf4663a734a5bcafe7f8d7af4667da949e7c0
boliwar <boliwar@gmail.com>
2022-12-05 03:59:59 +0200

    Миграция собственников

--- property/models.py
+++ property/models.py
@@ -74,10 +74,13 @@ class Complaint(models.Model):
         verbose_name_plural = 'Жалобы'
 
 class Owner(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
+    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField(region="CA", verbose_name='Нормализованный номер телефона', blank=True)
     owner_flats = models.ManyToManyField(Flat,related_name='flat_owners', verbose_name='Квартиры в собственности')
+
+    def __str__(self):
+        return f'{self.owner}'
 
     class Meta:
         verbose_name = 'Владелец'
