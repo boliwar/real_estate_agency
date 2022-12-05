7acf0426759c67e767cf5e316d1663c63c964a68
boliwar <boliwar@gmail.com>
2022-12-05 04:56:38 +0200

    Django: инлайны.

--- property/admin.py
+++ property/admin.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 
 from .models import Flat, Complaint, Owner
 
+class FlatInline(admin.TabularInline):
+    model = Owner.owner_flats.through
+    raw_id_fields = ('owner', 'flat')
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
     readonly_fields = ["created_at",]
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town' )
     list_editable = ('new_building',)
     list_filter = ('new_building', 'active', 'has_balcony', 'town', )
-    raw_id_fields = ('liked_users',)
+    raw_id_fields = ('liked_users', )
+    inlines = [FlatInline]
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('complaint_user','complaint_flat')
@@ -16,6 +22,7 @@ class ComplaintAdmin(admin.ModelAdmin):
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('owner_flats',)
     search_fields = ('owner',)
+    inlines = [FlatInline]
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Complaint, ComplaintAdmin)
