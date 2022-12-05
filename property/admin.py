e5d23a20abb666176943db6e87621b42c3e30acd
boliwar <boliwar@gmail.com>
2022-12-05 03:27:17 +0200

    привести телефонные номера в порядок

--- property/admin.py
+++ property/admin.py
@@ -5,7 +5,7 @@ from .models import Flat, Complaint
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ["created_at",]
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town' )
+    list_display = ('address', 'owner_pure_phone', 'price', 'new_building', 'construction_year', 'town' )
     list_editable = ('new_building',)
     list_filter = ('new_building', 'active', 'has_balcony', 'town', )
     raw_id_fields = ('liked_users',)
